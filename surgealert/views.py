6cb30808ecd91e4434b3d244dd8efc5ae083069a
bnocerini <bnocerini@gmail.com>
2015-04-08 16:18:53 -0400

    Update views.py

--- surgealert/views.py
+++ surgealert/views.py
@@ -6,7 +6,7 @@ from .models import Greeting
 
 # Create your views here.
 def index(request):
-	santamonica = {'start_latitude':'34.019454','start_longitude':'-118.491191','end_latitude':'34.019479','end_longitude':'-118.401718'}
+	athens = {'start_latitude':'33.957423','start_longitude':'-83.376847','end_latitude':'33.957423','end_longitude':'-83.376847'}
 	manhattanbeach = {'start_latitude':'33.881728','start_longitude':'-118.404121','end_latitude':'34.019479','end_longitude':'-118.401718'}
 	westhollywood = {'start_latitude':'34.085036','start_longitude':'-118.359318','end_latitude':'34.019479','end_longitude':'-118.401718'}
 	koreatown = {'start_latitude':'34.069112','start_longitude':'-118.281384','end_latitude':'34.019479','end_longitude':'-118.401718'}
@@ -16,7 +16,7 @@ def index(request):
 	northhollywood = {'start_latitude':'34.161161','start_longitude':'-118.358803','end_latitude':'34.019479','end_longitude':'-118.401718'}
 
 	cities = []
-	cities.append(santamonica)
+	cities.append(athens)
 	cities.append(manhattanbeach)
 	cities.append(westhollywood)
 	cities.append(koreatown)
@@ -25,7 +25,7 @@ def index(request):
 	cities.append(glendale)
 	cities.append(northhollywood)
 	info = ''
-	names = ["Athens Monica","Manhattan Beach","West Hollywood","Koreatown","Midwilshire","Downtown","Glendale","North Hollywood"]
+	names = ["Athens","Manhattan Beach","West Hollywood","Koreatown","Midwilshire","Downtown","Glendale","North Hollywood"]
 
 	for i in cities:
 		payload = cities[cities.index(i)]
